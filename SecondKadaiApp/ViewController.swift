29af685e759d3bdb6fcd22aa3f881edea9da7a45
masato.achiwa <tokemdokea@yahoo.co.jp>
2019-05-17 13:56:48 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by アプリ開発 on 2019/05/17.
+//  Copyright © 2019 Masato.achiwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
